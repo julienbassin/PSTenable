a6abdcdb614fbcdab9e57f69583b129c3a51f6b4
Jonathan Moss <jmoss@Jonathans-MacBook-Air.local>
2019-07-04 15:59:37 -0400

    Added comments/notes

--- PSTenable/Public/Connect-PSTenable.ps1
+++ PSTenable/Public/Connect-PSTenable.ps1
@@ -1,14 +1,37 @@
 #Requires -Modules PSFramework
 function Connect-PSTenable {
+    <#
+    .SYNOPSIS
+        Connects to the Tenable API and sets credentials, token, web session, and Tenable Server using PSFramework.
+    .DESCRIPTION
+        This function provides a way to set the credentials, token, web session, and
+        tenable server that is used within PSTenable.
+    .EXAMPLE
+        PS C:\> $Cred = Get-Credential
+        PS C:\> Connect-PSTenable -Credential $Cred -TenableServer "tenable.domain.com/rest" -Register
+        This prompts for user credentials, and then, using Connect-PSTenable, sets the credentials,
+        token, web session, and the Tenable Server using PSFramework.
+    .INPUTS
+        None
+    .OUTPUTS
+        None
+    .NOTES
+        If Connect-PSTenable is not ran with the Register switch, then Connect-PSTenable
+        must be ran each time since Tenable requires a unique token for each session.
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Position = 0, mandatory = $true)]
         [PSCredential]
-        $Credential = (Get-Credential -Message "Please put in your username and password"),
+        $Credential,
 
-        [Parameter(Position = 1,mandatory = $true)]
+        [Parameter(Position = 1, mandatory = $true)]
         [string]
-        $TenableServer
+        $TenableServer,
+
+        [Parameter(Position = 2, mandatory = $false)]
+        [switch]
+        $Register
     )
 
     begin {
@@ -17,14 +40,47 @@ function Connect-PSTenable {
 
     process {
 
-        Set-PSFConfig -FullName "PSTenable.Server" -Value $TenableServer
-        Register-PSFConfig -FullName "PSTenable.Server"
-        Set-PSFconfig -FullName "PSTenable.Credential" -Value $Credential
-        Register-PSFConfig -FullName "PSTenable.Credential"
+        if ($PSBoundParameters.ContainsKey('Register')) {
+            Set-PSFConfig -FullName "PSTenable.Server" -Value $TenableServer
+            Set-PSFconfig -FullName "PSTenable.Credential" -Value $Credential
+            Register-PSFConfig -FullName "PSTenable.Server"
+            Register-PSFConfig -FullName "PSTenable.Credential"
+        }
+
+        # Credentials
+        $APICredential = @{
+            username       = (Get-PSFConfigValue -FullName 'PSTenable.Credential').UserName
+            password       = (Get-PSFConfigValue -FullName 'PSTenable.Credential').GetNetworkCredential().Password
+            releaseSession = "FALSE"
+        }
+
+        $SessionSplat = @{
+            URI             = "$(Get-PSFConfigValue -FullName 'PSTenable.Server')/token"
+            SessionVariable = "SCSession"
+            Method          = "Post"
+            ContentType     = "application/json"
+            Body            = (ConvertTo-Json $APICredential)
+            ErrorAction     = "Stop"
+        }
+
+        try {
+            $Session = Invoke-RestMethod @SessionSplat
+        } catch {
+            Stop-PSFFunction -Message "Username or Password is incorect." -ErrorRecord $_
+            return
+        }
     }
 
     end {
-        Write-PSFMessage -Level Verbose -Message "Successfully stored credentials for $($Credential.Username)"
-        Write-PSFMessage -Level Verbose -Message "Successfully stored Tenable Server $($TenableServer)"
+
+        if ($PSBoundParameters.ContainsKey('Register')) {
+            Set-PSFconfig -FullName "PSTenable.Token" -Value $Session.response.token
+            Set-PSFconfig -FullName "PSTenable.WebSession" -Value $SCSession
+            Register-PSFConfig -FullName "PSTenable.Token"
+            Register-PSFConfig -FullName "PSTenable.WebSession"
+        }
+
+        Set-PSFconfig -FullName "PSTenable.Token" -Value $Session.response.token
+        Set-PSFconfig -FullName "PSTenable.WebSession" -Value $SCSession
     }
 }
