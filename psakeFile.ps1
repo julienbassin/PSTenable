53b9fb31f152a03a75720fcfb11eb51f07aad889
Jonathan Moss <jmoss@Jonathans-MacBook-Air.local>
2019-07-06 16:29:30 -0400

    Attempt to publish by manually building publish psake

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -3,8 +3,19 @@ properties {
     # This modifies the default behavior from the "Build" task
     # in the PowerShellBuild shared psake task module
     $PSBPreference.Build.CompileModule = $false
+
+    $moduleName         = $env:BHProjectName
+    $manifest           = Import-PowerShellDataFile -Path $env:BHPSModuleManifest
+    $outputDir          = Join-Path -Path $ENV:BHProjectPath -ChildPath 'Output'
+    $outputModDir       = Join-Path -Path $outputDir -ChildPath $env:BHProjectName
+    $outputModVerDir    = Join-Path -Path $outputModDir -ChildPath $manifest.ModuleVersion
 }
 
 task default -depends Test
 
-task Publish -FromModule PowerShellBuild -Version '0.3.1'
+task Test -FromModule PowerShellBuild -Version '0.3.1'
+
+task Publish {
+    "Publishing version [$($outputModVerDir)] to PSGallery..."
+    Publish-Module -Path $outputModDir -NuGetApiKey $ENV:PSGALLERY_API_KEY -Repository PSGallery
+} -description 'Publish to PowerShellGallery'
