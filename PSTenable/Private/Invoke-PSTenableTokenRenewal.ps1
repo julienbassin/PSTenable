0c78f94fee3e4aad2a6b51aece7071af923dfce7
Jonathan Moss <jmoss@Jonathans-MacBook-Air.local>
2019-07-09 16:03:56 -0400

    Add feature to set and restore TLS

--- PSTenable/Private/Invoke-PSTenableTokenRenewal.ps1
+++ PSTenable/Private/Invoke-PSTenableTokenRenewal.ps1
@@ -18,32 +18,38 @@ Function Invoke-PSTenableTokenRenewal {
 
     )
 
-        # Credentials
-        $APICredential = @{
-            username       = (Get-PSFConfigValue -FullName 'PSTenable.Credential').UserName
-            password       = (Get-PSFConfigValue -FullName 'PSTenable.Credential').GetNetworkCredential().Password
-            releaseSession = "FALSE"
-        }
+    # Credentials
+    $APICredential = @{
+        username       = (Get-PSFConfigValue -FullName 'PSTenable.Credential').UserName
+        password       = (Get-PSFConfigValue -FullName 'PSTenable.Credential').GetNetworkCredential().Password
+        releaseSession = "FALSE"
+    }
 
-        $SessionSplat = @{
-            URI             = "$(Get-PSFConfigValue -FullName 'PSTenable.Server')/token"
-            SessionVariable = "SCSession"
-            Method          = "Post"
-            ContentType     = "application/json"
-            Body            = (ConvertTo-Json $APICredential)
-            ErrorAction     = "Stop"
-            ErrorVariable   = "TenableTokenError"
-        }
+    $SessionSplat = @{
+        URI             = "$(Get-PSFConfigValue -FullName 'PSTenable.Server')/token"
+        SessionVariable = "SCSession"
+        Method          = "Post"
+        ContentType     = "application/json"
+        Body            = (ConvertTo-Json $APICredential)
+        ErrorAction     = "Stop"
+        ErrorVariable   = "TenableTokenError"
+    }
 
-        try {
-            $Session = Invoke-RestMethod @SessionSplat
-        } catch {
-            if ($tenabletokenerror -match "Could not create SSL/TLS secure channel") {
-                Stop-PSFFunction -Message "TLS 1.2 is not configured in your PowerShell session. Please configure prior to continuing." -ErrorRecord $_
-                return
-            }
-        }
+    ## Enable TLS 1.2
+    ## Taken from https://github.com/potatoqualitee/kbupdate/blob/master/kbupdate.psm1
+    $currentProgressPref = $ProgressPreference
+    $ProgressPreference = "SilentlyContinue"
+    $currentVersionTls = [Net.ServicePointManager]::SecurityProtocol
+    $currentSupportableTls = [Math]::Max($currentVersionTls.value__, [Net.SecurityProtocolType]::Tls.value__)
+    $availableTls = [enum]::GetValues('Net.SecurityProtocolType') | Where-Object { $_ -gt $currentSupportableTls }
+    $availableTls | ForEach-Object {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_
+    }
 
-        Set-PSFconfig -FullName "PSTenable.Token" -Value $Session.response.token
+    $Session = Invoke-RestMethod @SessionSplat
+
+    [Net.ServicePointManager]::SecurityProtocol = $currentVersionTls
+    $ProgressPreference = $currentProgressPref
+
+    Set-PSFconfig -FullName "PSTenable.Token" -Value $Session.response.token
 }
-
