f74d21bb10dfa84f6b2f65a1bfe4da6b1d8172ba
Jonathan Moss <jmoss@Jonathans-MacBook-Air.local>
2019-07-10 22:08:04 -0400

    Updated to not publish if master branch or not !deploy

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -7,24 +7,39 @@ properties {
     $manifest = Import-PowerShellDataFile -Path $env:BHPSModuleManifest
     $outputDir = Join-Path -Path $ENV:BHProjectPath -ChildPath 'Output'
     $outputModDir = Join-Path -Path $outputDir -ChildPath $env:BHProjectName
-    $outputModVerDir = Join-Path -Path $outputModDir -ChildPath $manifest.ModuleVersion
 }
 
 task default -depends Test
 
 task Test -FromModule PowerShellBuild -Version '0.3.1'
 
-task PublishToPSGallery -action {
-    Try {
-        $Splat = @{
+task PublishToPSGallery -Action {
+
+    ## https://github.com/KevinMarquette/PSGraph/blob/master/BuildTasks/PublishModule.Task.ps1
+
+    if ($ENV:BHBranchName -eq "master" -and
+        $ENV:BHCommitMessage -eq "!deploy" -and
+        -not [string]::IsNullOrWhiteSpace($ENV:API)) {
+
+        $publishModuleSplat = @{
             Path        = $outputModDir
             NuGetApiKey = $env:API
+            Verbose     = $true
+            Force       = $true
             ErrorAction = 'Stop'
         }
+        "Files in module output:"
+        Get-ChildItem $outputModDir -Recurse -File | Select-Object -Expand FullName
 
-        Publish-Module @Splat
+        "Publishing [$Destination] to [Powershell Gallery]"
 
-    } Catch {
-        throw $_
+        Publish-Module @publishModuleSplat
+    }
+
+    else {
+        "Skipping deployment: To deploy, ensure that...`n" +
+        "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
+        "`t* The repository APIKey is defined in `$ENV:nugetapikey (Current: $(![string]::IsNullOrWhiteSpace($ENV:API)))) `n" +
+        "`t* This is not a pull request"
     }
 }
